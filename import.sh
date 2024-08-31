a169b32043dd53e028d5221c4ff8152cfd5aa870
Adebola Omoboya <adeomoboya@gmail.com>
2024-08-31 13:38:27 +0100

    scripts for migration

---
+++ import.sh
@@ -0,0 +1,7 @@
+mongoimport --db store --collection deliveryzones --file store.deliveryzones.json --jsonArray
+mongoimport --db store --collection orders --file store.orders.json --jsonArray
+mongoimport --db store --collection products --file store.products.json --jsonArray
+mongoimport --db store --collection users --file store.users.json --jsonArray
+mongoimport --db store --collection mails --file store.mails.json --jsonArray
+mongoimport --db store --collection comments --file store.comments.json --jsonArray
+mongoimport --db store --collection externalcomments --file store.externalcomments.json --jsonArray
